5efc364e920702be076c9fb754ba65472676006c
FichteFoll <fichtefoll2@googlemail.com>
2016-10-25 20:33:51 +0200

    Type hints for riot_api

--- riot_api.py
+++ riot_api.py
@@ -1,16 +1,22 @@
 import requests
+import typing as t
 
-api_key = None
+# Commonly used types (for type hints)
+Params = t.Dict[str, str]
+JSON = t.Dict[str, t.Any]
+
+
+api_key = None  # type: str
 
 HOST = "https://{endpoint}.api.pvp.net"
 
 
-def set_key(key):
+def set_key(key: str):
     global api_key
     api_key = key
 
 
-def _build_url(url_base: str, region: str, **kwargs):
+def _build_url(url_base: str, region: str, **kwargs: t.Any):
     if url_base.startswith("/"):
         url_base = HOST + url_base
     kwargs.setdefault('endpoint', region)
@@ -20,7 +26,7 @@ def _build_url(url_base: str, region: str, **kwargs):
     return url_base.format(**kwargs)
 
 
-def _get_data(url, params=None):
+def _get_data(url: str, params: Params = None) -> JSON:
     if not params:
         params = {}
     params.setdefault('api_key', api_key)
@@ -29,16 +35,16 @@ def _get_data(url, params=None):
     return r.json()
 
 
-def _staticdata(variant, params=None, region="euw"):
+def _staticdata(variant: str, params: Params = None, region="euw") -> JSON:
     url = _build_url("/api/lol/static-data/{region}/v1.2/{variant}",
                      region=region, variant=variant)
 
     return _get_data(url, params)
 
 
-def get_champions(params=None):
+def get_champions(params: Params = None) -> JSON:
     return _staticdata("champion", params)
 
 
-def get_versions():
+def get_versions() -> JSON:
     return _staticdata("versions")

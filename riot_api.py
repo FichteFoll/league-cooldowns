b8ace8406303b541abd8ab2d47b8158a1e43c32f
FichteFoll <fichtefoll2@googlemail.com>
2016-10-25 20:28:53 +0200

    Properly regionalize urls

--- riot_api.py
+++ riot_api.py
@@ -2,7 +2,7 @@ import requests
 
 api_key = None
 
-HOST = "https://global.api.pvp.net"
+HOST = "https://{endpoint}.api.pvp.net"
 
 
 def set_key(key):
@@ -10,17 +10,29 @@ def set_key(key):
     api_key = key
 
 
+def _build_url(url_base: str, region: str, **kwargs):
+    if url_base.startswith("/"):
+        url_base = HOST + url_base
+    kwargs.setdefault('endpoint', region)
+    kwargs.setdefault('region', region)
+    kwargs.setdefault('platform', Platform[region])
+
+    return url_base.format(**kwargs)
+
+
 def _get_data(url, params=None):
     if not params:
         params = {}
     params.setdefault('api_key', api_key)
+
     r = requests.get(url, params=params)
     return r.json()
 
 
 def _staticdata(variant, params=None, region="euw"):
-    url = ("{host}/api/lol/static-data/{region}/v1.2/{variant}"
-           .format(host=HOST, region=region, variant=variant))
+    url = _build_url("/api/lol/static-data/{region}/v1.2/{variant}",
+                     region=region, variant=variant)
+
     return _get_data(url, params)
 
 
